90295274880f8b3ed99ef922208a546e1bdcf302
Alex <lexwalmsley@gmail.com>
2019-01-05 16:13:03 -0500

    Static uniform epsilon

--- server/Models.py
+++ server/Models.py
@@ -7,7 +7,6 @@
 import numpy as np
 import pandas as pd
 import cvxpy as cvx
-from matplotlib import pyplot as plt
 from skopt import gp_minimize, forest_minimize, gbrt_minimize, dummy_minimize
 from scipy import sparse
 
@@ -32,15 +31,16 @@ def newton_k(x, k1, k2, k3):
         return newton_k(y, k1, k2, k3)
     
 def U_init(model):
-    priority = ["ec", "r1", "unmatched"]
+    priority = ["equity", "rank1", "unmatched"]
+    k1, k2, k3 = [0.7, 0.65, 0.6]
+    
     n = len(model.data)
-    qi = model.ei
+    qi = model.qi
     U = [
         lambda x: 1-1/qi.sum()*x if qi.sum() > 0 else np.ones(x.shape),
         lambda x: 1/n*x,
         lambda x: 1-1/n*x
     ]
-    k1, k2, k3 = [0.7, 0.65, 0.6]
     k = newton_k(-2, k1, k2, k3) if (k1+k2+k3) > 1 else newton_k(2, k1, k2, k3)
     K = [k1, k2, k3, k]
     return priority, U, K
@@ -61,7 +61,7 @@ def U_joint(X, U, K):
     return k1*u1(x1) + k2*u2(x2) + k3*u3(x3) + k*k1*k2*u1(x1)*u2(x2) + k*k1*k3*u1(x1)*u3(x3) + k*k2*k3*u2(x2)*u3(x3) + (k*k)*k1*k2*k3*u1(x1)*u2(x2)*u3(x3)
 
 def U_eval(x_star, model):
-    ei = model.ei
+    qi = model.qi
     U = model.U
     K = model.K
     priority = model.priority
@@ -69,11 +69,11 @@ def U_eval(x_star, model):
     n, m = model.data.shape
     X = x_star.reshape(n,m)
     
-    r1 = np.ones((n,1)).T.dot(X.dot(np.eye(1,m,0).T)).ravel()[0]
-    unmatched = np.ones((n,1)).T.dot(np.ones((n,1))-X.dot(np.ones((m,1)))).ravel()[0]
-    ec = ei.T.dot(X.dot(np.eye(1,m,2).T)+(np.ones((n,1))-X.dot(np.ones((m,1))))).ravel()[0]
-    
-    vals = pd.Series({"r1":r1, "unmatched":unmatched, "ec":ec}).loc[priority].values.reshape(-1,3)
+    vals = pd.Series({
+        "rank1": np.ones((n,1)).T.dot(X.dot(np.eye(1,m,0).T)).ravel()[0],
+        "unmatched": np.ones((n,1)).T.dot(np.ones((n,1))-X.dot(np.ones((m,1)))).ravel()[0],
+        "equity": qi.T.dot(X.dot(np.eye(1,m,2).T)+(np.ones((n,1))-X.dot(np.ones((m,1))))).ravel()[0]
+    }).loc[priority].values.reshape(-1,3)
     score = U_joint(vals, U, K)[0]
     return score, vals
 
@@ -168,20 +168,22 @@ class BO():
 
         self.data = data
         self.ticket_capacity = ticket_capacity
-        ei = prev_rankings.iloc[:, -1].values.flatten()
-        self.ei = np.array(((ei == 0) | (ei == 3)), dtype=np.float64)
+        qi = prev_rankings.iloc[:, -1].values.flatten()
+        self.qi = np.array(((qi == 0) | (qi == 3)), dtype=np.float64)
         
         self.priority, self.U, self.K = U_init(self)
 
         n, m = data.shape
         attrs = {
-            "r1":np.kron(np.ones(n), np.array([1,0,0])),
-            "ec":np.kron(self.ei, np.array([1,1,0])),
+            "rank1":np.kron(np.ones(n), np.array([1,0,0])),
+            "equity":np.kron(self.qi, np.array([1,1,0])),
             "unmatched":np.kron(np.ones(n), np.array([1,1,1]))
         }
         self.c1 = attrs[self.priority[0]]
         self.c2 = attrs[self.priority[1]]
         self.c3 = attrs[self.priority[2]]
+        
+        self.epsilon = np.random.uniform(0, 0.001, size=(data.shape)).flatten()
         
         self.iter = 0
 
@@ -190,9 +192,8 @@ class BO():
         data = self.data
         ticket_capacity = self.ticket_capacity
         priority = self.priority
-
-        epsilon = np.random.normal(0, 0.001, size=(data.shape)).flatten()
-        c = w[0]*self.c1 + w[1]*self.c2 + w[2]*self.c3 + epsilon
+        
+        c = w[0]*self.c1 + w[1]*self.c2 + w[2]*self.c3 + self.epsilon
         
         try:
             m_employees, x_star = ip(data, ticket_capacity, c)
@@ -222,7 +223,7 @@ class BO():
         y0 = []
         
         try:
-                gp_minimize(self.run, dimensions, n_calls=50, acq_func="EI", n_points=50000, noise=1e-5)
+                gp_minimize(self.run, dimensions, n_calls=50, acq_func="EI", n_points=50000, noise=1e-10)
             
         except:
             pass
